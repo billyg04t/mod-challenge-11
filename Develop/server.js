0157bd57cf929992bf9865979e189a3f7fbfeb86
Shaun Swinehart <shaun.leroyhigh@hotmail.com>
2023-09-14 22:25:37 -0500

    changed server.js to include cors

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to serve static files (HTML, CSS, JavaScript)
 app.use(express.static('public'));
 
-// Define your routes here
+// Enable CORS for development
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Define API routes here
+app.get('/api/notes', (req, res) => {
+  // Return the notes as JSON
+  const notes = [{ title: 'Note 1', text: 'This is note 1' }, { title: 'Note 2', text: 'This is note 2' }];
+  res.json(notes);
+});
 
 // Serve the index.html file
 app.get('/', (req, res) => {
@@ -20,15 +29,6 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-// Define your API routes here
-app.get('/api/notes', (req, res) => {
-  // Your API logic for fetching notes goes here
-  // Return the notes as JSON
-  const notes = [{ title: 'Note 1', text: 'This is note 1' }, { title: 'Note 2', text: 'This is note 2' }];
-  res.json(notes);
-});
-
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
